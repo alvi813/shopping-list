e4e27b33bd0d079e727d6793a30603463c374895
Victoriya <alvi813@yandex.ru>
2020-07-15 05:08:54 +0300

    delete inner style, add handlers for clicking on label and the exclamation button

--- src/components/shopping-list-item.js
+++ src/components/shopping-list-item.js
@@ -4,32 +4,61 @@ import "./shopping-list-item.css"
 
 
 class ShoppingListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: false,
+            important: false
+        };
+    }
+
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        });
+    };
+
+    onButtonExclamationClick = () => {
+        this.setState({
+            important: true
+        });
+    };
 
     render() {
-        const { label, important = false } = this.props;
+        const { label } = this.props;
 
-        const shoppingListItemStyle = {
-            color: important ? "red" : "black",
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        const { done } = this.state;
+        const { important } = this.state;
+
+        let classNames = "shopping-list-item";
+        if (done) {
+            classNames += " done";
+        }
+        if (important) {
+            classNames += " important";
+        }
 
 
         return (
-            <span className="shopping-list-item">
+            <span className={classNames}>
 
             <span
                 className="shopping-list-item-label"
-                style={shoppingListItemStyle}>
+                onClick = {this.onLabelClick}
+            >
             {label}
             </span>
 
             <button type="button"
-                    className="btn btn-outline-success btn-sm float-right">
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onButtonExclamationClick}
+            >
                 <i className="fa fa-exclamation"/>
             </button>
 
             <button type="button"
-                    className="btn btn-outline-danger btn-sm float-right">
+                    className="btn btn-outline-danger btn-sm float-right"
+            >
                 <i className="fa fa-trash-o"/>
             </button>
 
