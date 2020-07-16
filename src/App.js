483bff6354a506865387e95202e1f1453d78c0c7
Victoriya <alvi813@yandex.ru>
2020-07-16 06:54:41 +0300

    implementing deleting items from a list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
@@ -9,25 +9,48 @@ import "./App.css"
 
 
 
-const App = () => {
+class App extends Component {
 
-    const shoppingListData = [
-        {label: 'Pen', id: 1},
-        {label: 'Notebook', important: true, id: 2},
-        {label: 'Pencil', id: 3}
-    ];
+    state = {
+        shoppingListData: [
+            {label: 'Pen', id: 1},
+            {label: 'Notebook', important: true, id: 2},
+            {label: 'Pencil', id: 3}
+        ]
+    };
 
-    return (
-        <div className="shopping-list-app">
-            <AppHeader toBuy={1} done={1}/>
-            <div className="top-panel d-flex">
-                <SearchPanel/>
-                <ItemStatusFilter/>
+    deleteItem = (id) => {
+        this.setState(({shoppingListData}) => {
+            const idDel = shoppingListData.findIndex((el) => el.id === id);
+
+            const before = shoppingListData.slice(0,idDel);
+            const after = shoppingListData.slice(idDel + 1);
+
+            const newArray = [...before, ...after];
+
+
+            return {
+                shoppingListData: newArray
+            }
+        })
+    };
+
+    render() {
+
+        return (
+            <div className="shopping-list-app">
+                <AppHeader toBuy={1} done={1}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <ShoppingList
+                    data={this.state.shoppingListData}
+                    onDeleted={this.deleteItem()}/>
             </div>
-
-            <ShoppingList data={shoppingListData}/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
