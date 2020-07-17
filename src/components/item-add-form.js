6f6324f4000e5ea3af78ebe7f7c281122819092b
Victoriya <alvi813@yandex.ru>
2020-07-17 04:18:35 +0300

    add the ability to enter the desired items in the field and send them to the list

--- src/components/item-add-form.js
+++ src/components/item-add-form.js
@@ -6,16 +6,43 @@ import './item-add-form.css';
 
 class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value,
+            value: ''
+        });
+
+
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();    // the page doesn't reload and the entered data doesn't disappear
+        this.props.onItemAdded(this.state.label);
+    }
+
+
     render() {
         return (
-            <div className="item-add-form">
+            <form className="item-add-form d-flex"
+                onSubmit={this.onSubmit}>
+
+                <input
+                    type = "text"
+                    className = "form-control"
+                    onChange = {this.onLabelChange}
+                    placeholder = "Enter your needs here"
+                />
+
                 <button
                     className="btn btn-outline-secondary"
-                    onClick={() => this.props.onItemAdded('new item')}
                 >
-                    Add item
+                    Add
                 </button>
-            </div>
+            </form>
         )
     }
 }
